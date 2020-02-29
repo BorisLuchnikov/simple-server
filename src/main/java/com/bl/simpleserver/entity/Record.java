ada3b1089813f7920a5be6ac3e1ae6f7e5d7e3b5
Boris Luchnikov <boris.luchnikov@gmail.com>
2020-02-29 15:18:06 +0300

    init

--- src/main/java/com/bl/simpleserver/entity/Record.java
+++ src/main/java/com/bl/simpleserver/entity/Record.java
@@ -7,6 +7,8 @@ import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
@@ -20,6 +22,7 @@ import java.time.LocalDateTime;
 public class Record {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
 
